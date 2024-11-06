a09b24a8bdb02223f227d78022e40ddad478cd30
Tim Larsen <ss23664@bristol.ac.uk>
2024-11-06 01:15:28 +0000

    CRP as exposure

--- crp_as_exposure.R
+++ crp_as_exposure.R
@@ -48,117 +48,60 @@ crp_snps <- clump_data(crp_snps)
 ### IL6 ###
 # -----------------------------------------------------#
 
+outcome_il6_eqtlgen <- extract_outcome_data(crp_snps$SNP, "eqtl-a-ENSG00000136244")
 
-#Uk Biobank
-il6_ukb <- read.table("IL6/IL6_signif.txt", 
-                      header = F, 
-                      sep="")
+#Harmonise
+crp_dat <- harmonise_data(crp_snps, outcome_il6_eqtlgen)
 
-il6_ukb <- data.frame(setNames(il6_ukb, ukb_vars)) %>%
-  mutate(olink_target_fullname = "IL6_UKB")
+#Results
+crp_res <- mr(crp_dat)
+crp_res <- crp_res %>% mutate(exposure = "C-reactive protein levels")
 
+# -----------------------------------------------------#
+### Plots ###
+# -----------------------------------------------------#
 
-# Format data
-il6_ukb <- format_data(
-  il6_ukb,
-  type = "exposure",
-  snps = NULL,
-  header = TRUE,
-  phenotype_col = "olink_target_fullname",
-  snp_col = "rsid",
-  beta_col = "BETA",
-  se_col = "SE",
-  effect_allele_col = "A1",
-  other_allele_col = "A2",
-  pval_col = "P",
-  samplesize_col = "N",
-  id_col = "HGNC.symbol",
-  chr_col = "CHROM",
-  pos_col = "POS38",
-  gene_col = "ensembl_id",
-  eaf_col= "A1FREQ"
-)
+#save
+save_func <- function(file_name, plot_name)
+{
+  png(file_name, res=450, height=5500, width=3500)
+  print(plot_name)
+  dev.off()
+  
+}
 
-il6_ukb <- clump_data(il6_ukb)
+# Scatter plot
+p1 <- mr_scatter_plot(crp_res, crp_dat)
 
-#il6_ukb$rsid<- il6_ukb$SNP
-#il6_ukb$pval<- il6_ukb$pval.exposure
+length(p1)
+
+p1[[1]]
 
 
-#Clumping
-# 
-# exposure_clumped<- ieugwasr::ld_clump(
-#   dat = il6_ukb_exposure,
-#   clump_kb = 10000,
-#   clump_r2 = 0.01,
-#   clump_p = 0.99,
-#   pop = "EUR",
-#   bfile = "/path/REF_PANEL/EUR",
-#   plink_bin ="/hpcpath/plink-1.90/plink")
-# 
+# -----------------------------------------------------#
+### ggforestplot ###
+# -----------------------------------------------------#
 # 
 
+# Forest plot for each exposure
 
-#GTEx - Blood
-il6_gtex_blood <- read.table("IL6/IL6_gtex_blood.txt", 
-                             header = T, 
-                             sep="")
+crp_forest_plot <- forestplot(crp_res %>%
+                                 #filter(method == "Inverse variance weighted") %>% 
+                                 arrange(exposure),
+                               name = exposure,
+                               estimate = b,
+                               se = se,
+                               pvalue = pval,
+                               colour = method,
+                               xlab = "",
+                               #ylab = "Outcome",
+                               title = "CRP levels on IL6",
+                               logodds = FALSE
+) + theme(axis.text = element_text(size = 10),
+          axis.title.x = element_text(size = 10))
 
-il6_gtex_blood %>% colnames()
-il6_gtex_blood %>% head()
+crp_forest_plot
 
-il6_gtex_blood %>%
-  filter(p < (0.05/333)) %>%
-  nrow()
-# 0 
+save_func(paste0(output_folder,"/MR_CRP/CRP_exposure_forest_plot.png"), crp_forest_plot)
 
-
-#GTEx - Liver
-il6_gtex_liver <- read.table("IL6/IL6_liver.txt", 
-                             header = T, 
-                             sep="")
-
-il6_gtex_liver %>% colnames()
-il6_gtex_liver %>% head()
-
-il6_gtex_liver %>%
-  filter(p < (0.05/333)) %>%
-  nrow()
-# 0
-
-
-#GWAS - CHARGE
-il6_charge <- read.csv("IL6/charge_gwas_il6.csv") %>% 
-  janitor::clean_names() %>% mutate(phenotype = "IL6_GWAS")
-
-
-# Keep SNPs with p-value below threshold
-il6_charge <- il6_charge %>% 
-  filter(p_value < p_val_thresh)
-
-il6_charge %>% nrow()
-
-# Format data
-il6_charge <- format_data(
-  il6_charge,
-  type = "exposure",
-  snps = NULL,
-  header = TRUE,
-  phenotype_col = "phenotype",
-  snp_col = "snp_rs_id",
-  beta_col = "beta",
-  se_col = "se",
-  effect_allele_col = "effect_allele",
-  other_allele_col = "other_allele",
-  pval_col = "p_value",
-  samplesize_col = "n_discovery",
-  #id_col = "HGNC.symbol",
-  chr_col = "chromosome",
-  pos_col = "position_bp_gr_ch37",
-  gene_col = "gene_nearest_gene_s",
-  eaf_col= "effect_allele_frequency"
-)
-
-# Clump data
-il6_charge <- clump_data(il6_charge)
 
